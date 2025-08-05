d7c6e0855958311ae153fd250fe630c209540f65
pulgueta <retardix456@gmail.com>
2025-08-05 14:29:31 -0500

    refactor: create new apps

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,10 +1,12 @@
-import { sql } from "@vercel/postgres";
-import { drizzle } from "drizzle-orm/vercel-postgres";
+import { SQL } from "bun";
+
+import { drizzle } from "drizzle-orm/bun-sql";
 
 import * as schema from "./schema";
 
 export const db = drizzle({
-  client: sql,
+  client: new SQL(process.env.POSTGRES_URL ?? ""),
   schema,
   casing: "snake_case",
+  logger: true
 });
