324a7ffa67c1be6857abd47b3136d50f97dfaed5
pulgueta <retardix456@gmail.com>
2026-01-17 23:05:54 -0500

    feat: implement email cleanup cron job and integrate resend service for email handling

--- apps/convex/convex/emails.ts
+++ apps/convex/convex/emails.ts
@@ -1,5 +1,6 @@
 "use node";
 
+import { Resend } from "@convex-dev/resend";
 import {
   AppointmentCancelledEmail,
   AppointmentCreatedEmail,
@@ -9,20 +10,18 @@ import {
   PastAppointmentReminderEmail,
   RescheduleRequestAcceptEmail,
   RescheduleRequestDeniedEmail,
-  ResetPasswordEmail,
-  WelcomeEmail,
 } from "@panabarbero/emails/emails";
 import { render } from "@react-email/components";
 import { v } from "convex/values";
-import { UseSend } from "usesend-js";
-
-import { internal } from "./_generated/api";
+import { components, internal } from "./_generated/api";
 import { internalAction } from "./_generated/server";
 import { subjects } from "./notifications";
 
-const from = "Soporte de PanaBarbero <contacto@panabarbero.com>";
+export const from = "Soporte de PanaBarbero <contacto@panabarbero.com>";
 
-const useSend = new UseSend(process.env.USESEND_API_KEY);
+export const resend = new Resend(components.resend, {
+  testMode: false,
+});
 
 export const sendEmail = internalAction({
   args: {
@@ -30,27 +29,12 @@ export const sendEmail = internalAction({
     subject: v.string(),
     html: v.string(),
   },
-  handler: async (_, args) => {
-    await useSend.emails.send({
+  handler: async (ctx, args) => {
+    await resend.sendEmail(ctx, {
       to: args.to,
       from,
       subject: args.subject,
       html: args.html,
-    });
-  },
-});
-
-export const sendWelcomeEmail = internalAction({
-  args: {
-    to: v.string(),
-  },
-  handler: async (ctx, args) => {
-    const html = await render(WelcomeEmail({}));
-
-    await ctx.runAction(internal.emails.sendEmail, {
-      subject: "Bienvenido a PanaBarbero",
-      to: args.to,
-      html,
     });
   },
 });
@@ -258,26 +242,6 @@ export const sendBarberInvitationEmail = internalAction({
     await ctx.runAction(internal.emails.sendEmail, {
       to: args.to,
       subject: subjects.barber_invited,
-      html,
-    });
-  },
-});
-
-export const sendAuthResetPasswordEmail = internalAction({
-  args: {
-    to: v.string(),
-    resetUrl: v.string(),
-  },
-  handler: async (ctx, args) => {
-    const html = await render(
-      ResetPasswordEmail({
-        resetUrl: args.resetUrl,
-      }),
-    );
-
-    await ctx.runAction(internal.emails.sendEmail, {
-      to: args.to,
-      subject: "Restablece tu contraseña - PanaBarbero",
       html,
     });
   },
