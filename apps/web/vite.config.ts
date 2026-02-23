a3412afd99fc6e8b96709dc819e1fe27cc4a3806
pulgueta <retardix456@gmail.com>
2026-02-23 00:04:06 -0500

    feat: deploy to cloudflare

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,21 +1,15 @@
+import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
-import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
 const config = defineConfig({
   plugins: [
     devtools(),
-    nitro({
-      vercel: {
-        functions: {
-          runtime: "bun1.x",
-        },
-      },
-    }),
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
     viteTsConfigPaths({
       projects: ["./tsconfig.json"],
     }),
