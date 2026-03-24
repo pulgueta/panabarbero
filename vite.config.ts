b87dcf912f8db2826f940e519c195502e3f4ae03
pulgueta <retardix456@gmail.com>
2026-03-24 16:12:23 -0500

    refactor: move to other cloud

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
-import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 
 const config = defineConfig({
   plugins: [
     devtools(),
-    cloudflare({ viteEnvironment: { name: "ssr" } }),
+    nitro(),
     tailwindcss(),
     tanstackStart({
       sitemap: {
-        host: "https://www.panabarbero.com",
+        host: "https://panabarbero.com",
       },
     }),
     viteReact(),
