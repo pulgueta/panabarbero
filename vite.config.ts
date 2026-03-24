6a51bb638b66b127c211346453f579cbf551302d
pulgueta <retardix456@gmail.com>
2026-03-24 15:37:32 -0500

    refactor: move app to cloudflare workers

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,18 @@
+import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
-import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 
 const config = defineConfig({
   plugins: [
     devtools(),
-    nitro(),
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
     tailwindcss(),
     tanstackStart({
       sitemap: {
-        host: import.meta.env.PROD
-          ? "https://panabarbero.com"
-          : "http://localhost:3000",
+        host: "https://www.panabarbero.com",
       },
     }),
     viteReact(),
